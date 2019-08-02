fd41f5f55bee302e44582432bdd765c1c81c874d
Sikdar, Kalyan <kalyan.sikdar@honeywell.com>
2019-08-02 16:34:09 -0400

    Introduced Consumer Functional Interface

---
+++ src/Unit2/StandardFunctionalInterfaceExample.java
@@ -0,0 +1,43 @@
+package Unit2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import Common.Person;
+
+public class StandardFunctionalInterfaceExample {
+    public static void main(String[] args){
+        List<Person> people = Arrays.asList(
+                new Person("Charles", "Dickens", 60),
+                new Person("Lewis", "Carroll", 42),
+                new Person("Thomas", "Carlyle", 51),
+                new Person("Charlotte", "Bronte", 45),
+                new Person("Matthew", "Arnold", 39)
+        );
+
+        Collections.sort(people, (person1, person2) -> person1.getLastName().compareTo(person2.getLastName()));
+//        Collections.sort(people, Comparator.comparing(Person::getLastName));      // alternate method
+
+        System.out.println("Print all people... ");
+        printConditionally(people, person -> true, person -> System.out.println(person));
+
+        System.out.println("Print all people whose last name starts with C... ");
+        printConditionally(people, person -> person.getLastName().startsWith("C"), person -> System.out.println(person.getLastName()));
+
+        System.out.println("Print all people whose last name ends with E...");
+        printConditionally(people, person -> person.getLastName().endsWith("e"), person -> System.out.println(person.getFirstName()
+                + " " + person.getLastName()));
+    }
+
+    private static void printConditionally(List<Person> people, Predicate<Person> predicate, Consumer<Person> consumer) {
+        for (Person person: people) {
+            if (predicate.test(person)) {
+                consumer.accept(person);
+            }
+        }
+    }
+}
+
